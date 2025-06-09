bdb7e4ae88aa33687a8c04bec4a384ecc95f400a
Akram-00 <aliakram9789@gmail.com>
2025-06-09 20:58:00 +0530

    WhoChoose Component Completed

--- src/components/Homepage/Home.tsx
+++ src/components/Homepage/Home.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import WhyChoose from './WhyChoose'
-import SmoothPlusLayout from './check'
 
 export default function HomeComponent() {
   return (
     <main className=''>
       <HeroSection />
       <WhyChoose />
-      <SmoothPlusLayout/>
     </main>
   )
 }
