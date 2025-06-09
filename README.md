c42ec3c83a38a80794525c6180b599106c1139eb
Rasmi142 <rasmioff@gmail.com>
2025-06-09 21:04:18 +0530

    homepage modified

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ npm run dev
 yarn dev
 # or
 pnpm dev
-# or
+# orcd
 bun dev
 ```
 
--- src/components/Homepage/Home.tsx
+++ src/components/Homepage/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import WhyChoose from './WhyChoose'
+import MakeItYourOwnLanding from './MakeItYourOwnLanding'
 
 export default function HomeComponent() {
   return (
     <main className=''>
       <HeroSection />
       <WhyChoose />
+      <MakeItYourOwnLanding/>
     </main>
   )
 }
